726543d60220f2cb17b70c07b11128f76931e6d6
Yikun Wu <yikunw@kth.se>
2025-09-30 10:58:29 +0200

    launch done v0, SM: import, init, service subscription done

--- src/irob_assignment_5/irob_assignment_5/SM_students.py
+++ src/irob_assignment_5/irob_assignment_5/SM_students.py
@@ -1,6 +1,18 @@
 #!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
 
+from GetGoal.srv import GetGoal
+from Activate.srv import Activate
+from Deactivate.srv import Deactivate
+from AtGoal.srv import AtGoal
+from geometry_msgs.msg import Twist
+from nav_msgs.msg import Odometry
+from sensor_msgs.msg import LaserScan
+from std_msgs.msg import Bool
 
+import math
+import time
 #necessary imports here , follow basic Ros2 node stuff
 
 
@@ -15,17 +27,68 @@ class SMStudentsNode(Node):
     def __init__(self):
         super().__init__('SM_students_node')
 
+        #clients
+        self.activate_client = self.create_client(Activate, 'activate')
+        self.deactivate_client = self.create_client(Deactivate, 'deactivate')
+        self.get_goal_client = self.create_client(GetGoal, 'get_goal')
+        self.at_goal_client = self.create_client(AtGoal, 'at_goal')
+        
+        #publisher
+        self.cmd_vel_pub = self.create_publisher(Twist, '/cmd_vel', 10)
+
+        #subscriber
+        self.odom_sub = self.create_subscription(Odometry, '/odom', self.odom_callback, 10)
+        self.scan_sub = self.create_subscription(LaserScan, '/scan', self.scan_callback, 10)
+        self.active_sub = self.create_subscription(Bool, '/robot_active', self.active_callback, 10)
+
+        #services
+        self.activate_sm_srv = self.create_service(Activate, 'activate_sm', self.handle_activate_robot)
+        self.deactivate_sm_srv = self.create_service(Deactivate, 'deactivate_sm', self.handle_deactivate_robot)
+
+        #variables
+        self.current_pose = None
+        self.current_orientation = None        
+        self.current_goal = None
+        self.active = False
+
+        #state machine
+        self.state = "INACTIVE"
+
         #add important initializations here
         self.get_logger().info("SM_students_node ready. Robot is inactive until activated.")
 
+    def odom_callback(self, msg):
+        self.current_pose = (msg.pose.pose.position.x, msg.pose.pose.position.y)
+        self.current_orientation = msg.pose.pose.orientation
+
+    def active_callback(self, msg):
+        self.active = msg.data
+
+    def scan_callback(self, msg):
+        self.scan_data = msg
+
     def handle_activate_robot(self, request, response):
-        pass
         #Do some awesome stuff here!
-        
-        
+        activate_response = self.activate_client.call(Activate.Request())
+        self.get_logger().info(activate_response.message)
+        self.state = "GET_GOAL"
+        response.success = True
+        response.message = "State machine activated."
+        return response    
+
+    def handle_deactivate_robot(self, request, response):
+        deactivate_response = self.deactivate_client.call(Deactivate.Request())
+        self.get_logger().info(deactivate_response.message)
+        #should we set 0 velocity by ourselves?
+        self.state = "INACTIVE"
+        response.success = True
+        response.message = "State machine deactivated."
+        return response
+
 def main(args=None):
     pass
     # what to do here?
+    
 
 
 if __name__ == '__main__':
--- src/irob_assignment_5/launch/project_launch.launch.py
+++ src/irob_assignment_5/launch/project_launch.launch.py
@@ -1,14 +1,39 @@
-
 #Add necessary imports
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from ament_index_python.packages import get_package_share_directory
+import os
 
+
+def generate_launch_description():
 #USE:
 use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     
-#pkg_irob_assignment_5 = "/home/parag/ros_as1_test/src/irob_assignment_5"
+pkg_irob_assignment_5 = "/home/y/i/yikunw/DD2410_5/src/irob_assignment_5"
 #pkg_irob_assignment_5 = get_package_share_directory('irob_assignment_5') #better way to do above!
 
 # Launch node/executable goal_server , add parameter goal_params_SM.yaml or goal_params_BT.yaml as needed 
 # Launch node/executable activation_server
 # Launch node/executable at_goal_server 
+    pkg_share = get_package_share_directory('<your_package_name>')
+    goals_param = os.path.join(pkg_share, 'config', 'goals_set.yaml')
 
+    return LaunchDescription([
+        Node(
+            package='irob_assignment_5',
+            executable='goal_server.py',
+            name='goal_service_node',
+            parameters=[goals_param]
+        ),
+        Node(
+            package='irob_assignment_5',
+            executable='at_goal_server.py',
+            name='at_goal_service_node'
+        ),
+        Node(
+            package='irob_assignment_5',
+            executable='activation_server.py',
+            name='activation_server'
+        )
+    ])
 
