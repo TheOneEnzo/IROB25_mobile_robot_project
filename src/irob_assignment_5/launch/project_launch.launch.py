97ccc6fbd3ef0789357c29a4c19a89376a111f16
Yikun Wu <yikunw@kth.se>
2025-10-06 11:04:46 +0200

    fixed launch file

--- src/irob_assignment_5/launch/project_launch.launch.py
+++ src/irob_assignment_5/launch/project_launch.launch.py
@@ -1,40 +1,66 @@
 #Add necessary imports
-from launch import LaunchDescription
-from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
 import os
 
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
 
-def generate_launch_description():
 #USE:
 use_sim_time = LaunchConfiguration('use_sim_time', default='true')
     
-pkg_irob_assignment_5 = "/home/y/i/yikunw/DD2410_5/src/irob_assignment_5"
+#pkg_irob_assignment_5 = "/home/parag/ros_as1_test/src/irob_assignment_5"
 #pkg_irob_assignment_5 = get_package_share_directory('irob_assignment_5') #better way to do above!
 
 # Launch node/executable goal_server , add parameter goal_params_SM.yaml or goal_params_BT.yaml as needed 
 # Launch node/executable activation_server
 # Launch node/executable at_goal_server 
-    pkg_share = get_package_share_directory('<your_package_name>')
-    goals_param = os.path.join(pkg_share, 'config', 'goals_params_SM.yaml')
-    #!!! have to change different params files when dealing with different tasks!
+
+
+def generate_launch_description():
+    # Get the package share directory
+    pkg_share = get_package_share_directory('irob_assignment_5')
+
+    # Path to the goal parameters YAML file
+    goal_params_file_path = os.path.join(pkg_share, 'config', 'goal_params_SM.yaml')
+
+    # Goal Server Node with parameters
+    goal_server_node = Node(
+        package='irob_assignment_5',
+        executable='goal_server',
+        name='goal_server',
+        output='screen',
+        parameters=[goal_params_file_path]
+    )
+
+    # Activation Server Node
+    activation_server_node = Node(
+        package='irob_assignment_5',
+        executable='activation_server',
+        name='activation_server',
+        output='screen'
+    )
+
+    # At Goal Server Node
+    at_goal_server_node = Node(
+        package='irob_assignment_5',
+        executable='at_goal_server',
+        name='at_goal_server',
+        output='screen'
+    )
+
+    # Your State Machine Node
+    sm_students_node = Node(
+        package='irob_assignment_5',
+        executable='SM_students',
+        name='SM_students_node',
+        output='screen'
+    )
 
     return LaunchDescription([
-        Node(
-            package='irob_assignment_5',
-            executable='goal_server.py',
-            name='goal_service_node',
-            parameters=[goals_param]
-        ),
-        Node(
-            package='irob_assignment_5',
-            executable='at_goal_server.py',
-            name='at_goal_service_node'
-        ),
-        Node(
-            package='irob_assignment_5',
-            executable='activation_server.py',
-            name='activation_server'
-        )
+        goal_server_node,
+        activation_server_node,
+        at_goal_server_node,
+        sm_students_node
     ])
 
